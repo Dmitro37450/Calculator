d0421e79d7445db5a3c442d3b6e4c63f0808baa4
Dmitro37450 <dimakvl454@gmail.com>
2025-05-15 23:16:02 +0300

    core function created

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-int result;
+int result = 0;
 int input;
 char operation;
 
@@ -21,6 +21,45 @@ static int division(int y, int x = 0) {
     return y / x;
 }
 
+// Core function
+do {
+    Console.Write("Input number 1: ");
+    result = Convert.ToInt16(Console.ReadLine());
+    Console.Write("Input number 2: ");
+    input = Convert.ToInt16(Console.ReadLine());
+    
+    bool valid = false;
+    do {
+        Console.Clear();
+        Console.WriteLine(Convert.ToString(result));
+        Console.WriteLine("Input: " + Convert.ToString(input) + '\n');
+        Console.WriteLine("[+]: Addition");
+        Console.WriteLine("[-]: Subtraction");
+        Console.WriteLine("[*]: Multiplication");
+        Console.WriteLine("[/]: Division");
+
+        Console.Write("Choose operation: ");
+        operation = Console.ReadKey().KeyChar;
+
+        switch (operation)
+        {
+            default:
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\nInvalid operation!");
+            Console.ResetColor();
+            Console.WriteLine("Press anything to continue.");
+            Console.ReadKey();
+            break;
+        }
+
+        if (valid) {
+            valid = false;
+        }
+
+    } while (!valid);
+    
+} while(true);
+/*
 Console.Write("Input number 1: ");
 result = Convert.ToInt16(Console.ReadLine());
 Console.Write("Input number 2: ");
@@ -29,3 +68,4 @@ input = Convert.ToInt16(Console.ReadLine());
 Console.WriteLine(Convert.ToString(division(input, result)));
 
 // Console.WriteLine(Convert.ToString(plus()));
+*/

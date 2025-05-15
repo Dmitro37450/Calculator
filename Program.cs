968658b944d0447392b595d38386da86b89728a5
Dmitro37450 <dimakvl454@gmail.com>
2025-05-15 21:33:01 +0300

    basic functions

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int result;
+int input;
+char operation;
+
+static int addition(int y, int x = 0) {
+    return x + y;
+};
+
+static int subtraction(int y, int x = 0) {
+    return x - y;
+}
+
+static int multiplication(int y, int x = 0) {
+    return x * y;
+}
+
+static int division(int y, int x = 0) {
+    if(x==0) return 0;
+    return y / x;
+}
+
+Console.Write("Input number 1: ");
+result = Convert.ToInt16(Console.ReadLine());
+Console.Write("Input number 2: ");
+input = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine(Convert.ToString(division(input, result)));
+
+// Console.WriteLine(Convert.ToString(plus()));
